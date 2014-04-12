b410a32fb2fa5adc4f76723dfcefdcd94d3fcc0b
Rolf Erik Lekang <me@rolflekang.com>
2014-04-12 16:52:43 -0700

    load plugins by config
    The plugins are now loaded if their are in the plugins config. For
    example to load the op plugin it is required for the config to contain:
    
    plugins:
      op:

--- itera-bot.rb
+++ itera-bot.rb
@@ -5,15 +5,29 @@ require 'yaml'
 
 require_relative 'lib/override_cinch_irc'
 
-require_relative 'plugins/http_server'
-require_relative 'plugins/github_commits'
-require_relative 'plugins/op'
-
 config_file = File.join(File.dirname(__FILE__), 'config.yml')
 
 raise "No configuration file found!" unless File.exists?(config_file)
 
 config = YAML.load_file(config_file)
+
+config['plugins'].each do |plugin_config|
+  require_relative "plugins/#{plugin_config[0]}"
+end
+
+def list_plugins(plugins)
+  plugin_objects = [] 
+  plugins.each do |plugin_config|
+    plugin = get_plugin(plugin_config[0])
+    plugin_objects.push(plugin) if plugin
+  end
+  return plugin_objects
+end
+
+def get_plugin(plugin_name)
+  return GithubCommits if plugin_name == 'github_commits'
+  return OP if plugin_name == 'op'
+end
 
 Signal.trap('HUP') do
   ENV['IRC_SERVER_FD'] = @bot.irc.socket.fileno.to_s
@@ -26,10 +40,12 @@ end
     c.server          = config['server']
     c.channels        = config['channels']
     c.fd              = ENV['IRC_SERVER_FD'].to_i
-    c.plugins.plugins = [GithubCommits, OP]
+    c.plugins.plugins = list_plugins(config['plugins']) 
 
-    c.plugins.options[GithubCommits] = config['plugins']['github_commits']
-    c.plugins.options[OP]            = config['plugins']['op']
+    config['plugins'].each do |plugin_config|
+      plugin = get_plugin(plugin_config[0])
+      c.plugins.options[plugin] = plugin_config[1] if plugin
+    end
   end
 end
 
